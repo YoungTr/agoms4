27f52fb38417f723860fa6eb15126e78b7508667
tangyan <tangyan1004@gmail.com>
2021-06-21 23:06:27 +0800

    BST

--- src/com/agoms4/c3searching/c2binarysearchtree/BST.java
+++ src/com/agoms4/c3searching/c2binarysearchtree/BST.java
@@ -71,6 +71,20 @@ public class BST<Key extends Comparable<Key>, Vale> {
         return min(x.left);
     }
 
+    public Key floor(Key key) {
+        Node x = floor(root, key);
+        if (x == null) return null;
+        return x.key;
+    }
 
+    private Node floor(Node x, Key key) {
+        if (x == null)  return null;
+        int cmp = key.compareTo(x.key);
+        if (cmp == 0)   return x;
+        if (cmp < 0)    return floor(x.left, key);
+        Node t = floor(x.right, key);
+        if (t != null)  return t;
+        else            return x;
+    }
 
 }
